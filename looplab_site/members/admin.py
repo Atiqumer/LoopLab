c4e32968d8ed5b752a7eda8846de4400b4b8bd05
Atiq Umer <atiqumer15@gmail.com>
2025-11-15 10:45:05 +0500

    members_app's model creation

--- looplab_site/members/admin.py
+++ looplab_site/members/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+# members/admin.py
+from django.contrib import admin
+from .models import Contributor 
+
+@admin.register(Contributor)
+class ContributorAdmin(admin.ModelAdmin):
+    list_display = ('name', 'bio', 'profile_photo') 
+    search_fields = ('name', 'bio')
--- looplab_site/members/models.py
+++ looplab_site/members/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Contributor(models.Model):
+    name = models.CharField(max_length=100)
+    bio = models.TextField(
+        blank=True,
+        help_text="Short bio or description of their role/skills."
+    )
+    profile_photo = models.ImageField(
+        upload_to='contributor_photos/',
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return self.name
